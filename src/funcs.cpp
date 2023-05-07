56a23524acb14a21b01f32dedd69cc5d99bfc849
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-07 14:44:40 +0300

    Add more comments and modify funcs.cpp

--- src/funcs.cpp
+++ src/funcs.cpp
@@ -1,31 +1,33 @@
-//Include header files
+// Include header files
 #include "../header/funcs.h"
-
-//Include libraries
+// Include libraries
 #include <fstream>
 #include <string>
 #include <cctype>
 
 void formatFile(std::string inputFilename, std::string outputFilename) {
+    // Open input and output files
     std::ifstream inputFile;
     std::ofstream outputFile;
     inputFile.open(inputFilename);
     outputFile.open(outputFilename);
-    if (!inputFile) {
-        outputFile << "Error in reading the test file!";
-    } else {
-        //Remove punctuation from the text and convert all characters to lowercase
+    
+    if (inputFile.is_open()){   // Make sure there are no errors in opening the file
+        // Remove punctuation from the text and convert all characters to lowercase
         std::string line;
-        while (std::getline(inputFile, line)) { //Loop through each line of the file
-            for (char c : line) { //Loop through each character of the line
-                if (!std::ispunct(c)) { //If the character is not punctuation, convert it to lowercase and print it
+        while (std::getline(inputFile, line)) { // Loop through each line of the file
+            for (char c : line) { // Loop through each character of the line
+                if (!std::ispunct(c)) { // If the character is not punctuation, convert it to lowercase and print it
                     c=std::tolower(c);
-                    outputFile << c;
+                    outputFile << std::tolower(c);
                 }
             }
             outputFile << std::endl;
         }
+    } else {
+        outputFile << "Error in reading the test file!";
     }
+
     inputFile.close();
     outputFile.close();
 }
