2bcb772d09fe8d9b67f3ace29bbbce11fb03d1c0
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-27 13:37:33 +0300

    Add Structs.h file.

---
+++ header/Structs.h
@@ -0,0 +1,8 @@
+// includes
+#include <iostream>
+
+typedef struct Pair {
+    std::string word1;   // first word of the pair
+    std::string word2;   // second word of the pair
+    int apps;   // number of appearances
+} pair ;
