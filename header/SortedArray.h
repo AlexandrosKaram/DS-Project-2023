28701b94411833755cec589c0ff660007567ce6e
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-27 16:30:57 +0300

    Add SortedArray class files (incomplete).

--- header/SortedArray.h
+++ header/SortedArray.h
@@ -1,1 +1,20 @@
 # pragma once
+
+// include header files
+#include "Array.h"
+#include "Structs.h"
+// include libraries
+#include <iostream>
+
+// declare SortedArray class
+class SortedArray: public Array {   // derived class of Array
+    public:
+        // constructors - destructors
+        SortedArray();
+        SortedArray(int rows);
+        ~SortedArray();
+        // methods
+        void quicksortArray();  // quick sort the array
+        void swapPairs(pair& a, pair& b);   // swaps two pairs
+        virtual int timesExists(int i);   // returns number of appearances
+};
--- src/SortedArray.cpp
+++ src/SortedArray.cpp
@@ -1,1 +1,40 @@
+// include header files
 #include "../header/SortedArray.h"
+#include "../header/Array.h"
+// include libraries
+#include <iostream>
+
+SortedArray:: SortedArray() : Array() {};   // default constructor
+
+SortedArray:: SortedArray(int rows) : Array(rows) {};   // construcot that creates an array with specific number of rows
+
+SortedArray:: ~SortedArray() {};   // destructor
+
+void SortedArray:: swapPairs(pair& a, pair& b) {
+    pair temp = a;
+    a = b;
+    b = temp;
+}
+
+int SortedArray:: timesExists(int i) {   // returns number of appearances
+    int cnt = 0;   // counter
+    bool flag = true;
+    // loop only through the pairs around, since array is sorted
+    for (int j=i-1 ; j>=0 && flag ; j++) {
+        if (isSamePair(i, j)) {
+            cnt++;   // if pairs are the same increase counter
+        } else {
+            flag = false;   // stop looping
+        }
+    }
+    flag = true;
+    for (int j=i+1 ; j<rows ; j++) {
+        if (isSamePair(i, j)) {
+            cnt++;   // if pairs are the same increase counter
+        } else {
+            flag = false;
+        }
+    }
+
+    return cnt;   // return counter
+}
