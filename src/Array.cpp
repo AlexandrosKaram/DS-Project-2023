d1808f5e3f73221db4ebefada4526588932302c7
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-27 13:13:51 +0300

    Change comments on Array.cpp.

--- src/Array.cpp
+++ src/Array.cpp
@@ -2,22 +2,22 @@
 
 #include <string>
 
-Array::Array() {}; // Empty constructor
+Array::Array() {};   // empty constructor
 
 Array::Array(int rows) {
-    this->rows = rows;
-    data = new std::string*[rows]; // Allocate memory for the rows
+    this->rows = rows; 
+    data = new std::string*[rows];   // allocate memory for the rows
     for (int i = 0;i < rows;i++) {
-        data[i] = new std::string[3]; // Allocate memory for the three columns of each row
-        data[i][2] = '0'; // Initialize appearances to zero.
+        data[i] = new std::string[3];   // allocate memory for the three columns of each row
+        data[i][2] = '0';   // initialize appearances to zero.
     }
 }
 
 Array::~Array() {
     for (int i = 0;i < rows;i++) {
-        delete[] data[3]; // Deallocate memory for the three columns of each row
+        delete[] data[3];   // deallocate memory for the three columns of each row
     }
-    delete[] data; // Deallocate memory for the rows
+    delete[] data;   // deallocate memory for the rows
 }
 
 void Array::setWord1(int i, std::string word1) {
@@ -29,7 +29,7 @@ void Array::setWord2(int i, std::string word2) {
 }
 
 void Array::setAppearances(int i, int n) {
-    data[i][2] = std::to_string(n); // Int to string
+    data[i][2] = std::to_string(n);   // int to string
 }
 
 std::string Array::getWord1(int i) {
@@ -41,7 +41,7 @@ std::string Array::getWord2(int i) {
 }
 
 int Array::getAppearances(int i) {
-    return stoi(data[i][2]); // String to int
+    return stoi(data[i][2]);   // string to int
 }
 
 int Array::getRows() {
