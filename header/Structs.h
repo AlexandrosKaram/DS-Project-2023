03ec288d8d5cbbc923253b17df5559c2ba343a9a
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-27 16:28:28 +0300

    Add overloads in Structs.h .

--- header/Structs.h
+++ header/Structs.h
@@ -8,3 +8,29 @@ typedef struct Pair {
     std::string word2;   // second word of the pair
     int apps;   // number of appearances
 } pair ;
+
+// overloads
+// bool operators
+bool operator==(pair& p1, pair& p2) {
+    return (p1.word1 == p2.word1) && (p1.word2 == p2.word2);
+}
+
+bool operator>(pair& p1, pair& p2) {
+    return p1.word1 > p2.word1 ||
+           p1.word1 == p2.word1 && p1.word2 > p2.word2;
+}
+
+bool operator>=(pair& p1, pair& p2) {
+    return p1.word1 >= p2.word1 ||
+           p1.word1 == p2.word1 && p1.word2 >= p2.word2;
+}
+
+bool operator<(pair& p1, pair& p2) {
+    return p1.word1 < p2.word1 ||
+           p1.word1 == p2.word1 && p1.word2 < p2.word2;
+}
+
+bool operator<=(pair& p1, pair& p2) {
+    return p1.word1 <= p2.word1 ||
+           p1.word1 == p2.word1 && p1.word2 <= p2.word2;
+}
