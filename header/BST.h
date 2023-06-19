9167af6ab1902f7df9e17d6579c9c4b9aebe2e47
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-06-19 14:48:26 +0300

    Add methods to BST.h.

--- header/BST.h
+++ header/BST.h
@@ -3,17 +3,24 @@
 #include "Pair.h"
 #include "File.h"
 
+class Node {
+    public:
+    Pair data;
+    Node* left;
+    Node* right;
+    // constructor 
+    Node(Pair value) {this->data = value; left=NULL; right=NULL;}
+};
 class BST {
     protected:
-    typedef struct Node {
-        Pair value;
-        Node *left, *right;
-    } Node;
     Node* root;
     // helper methods
     void destroyTree(Node* node);
     void insertNode(Node*& node, Pair value);
     void increaseCount(Node* node, Pair value);
+    // create and search the pairs
+    void createPairs(File formatted, Pair* Qset, int QsetSize);
+    void searchPairs();
 
     public:
     // constructor - destructor
@@ -22,4 +29,5 @@ class BST {
     // methods
     void insert(Pair value);
     void search(Pair value);
+    void showResults(File formatted, Pair* Qset, int QsetSize);
 };
--- src/BST.cpp
+++ src/BST.cpp
@@ -1,3 +1,27 @@
 #include "../header/BST.h"
 #include <iostream>
 #include <chrono>
+
+BST:: BST() {
+    root = NULL;
+}
+
+BST:: ~BST() {
+    destroyTree(root);
+}
+
+// recursively destroy the tree
+void BST:: destroyTree(Node* node) {
+    if (node==NULL) return;
+    // destroy left subtree
+    destroyTree(node->left);
+    // destroy right subtree
+    destroyTree(node->right);
+    // destroy the node itself
+    delete node;
+}
+
+void BST:: insertNode(Node*& node, Pair value) {
+    if (node == NULL)
+        node = new Node(value); 
+}
