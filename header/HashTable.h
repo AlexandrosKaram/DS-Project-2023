3a932ecf50352eb90958c5a168825f9659be7d3e
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-06-24 15:22:37 +0300

    Add HashTable.h file.

--- header/HashTable.h
+++ header/HashTable.h
@@ -1,1 +1,33 @@
 # pragma once
+
+#include <iostream>
+#include "Pair.h"
+
+class hashEntry {
+    public:
+    Pair pair;
+    int hashCode;
+    hashEntry() {hashCode = NULL;}
+};
+
+class HashTable {
+    protected:
+    hashEntry* data;
+    int size, capacity;
+    int hashFunction(hashEntry key);
+    double constTime, searchTime;
+    // private helper methods
+    void createPairs(std::string filename);
+    void searchPairs(Pair* Qset, int QsetSize);
+    // private methods
+    void doubleSize();
+    void handlePair(Pair* tempPair);
+    int createHashCode(Pair tempPair);
+
+    public:
+    // constructor - destructor
+    HashTable();
+    ~HashTable();
+    // methods
+    void showResults(std::string filename, Pair* Qset, int QsetSize);
+};
