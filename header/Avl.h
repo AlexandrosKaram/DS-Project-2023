48470d04d1af640d4b171e216947aeadeed41936
iHaz32 <xaralamposdelig@gmail.com>
2023-06-25 17:49:02 +0300

    Initialize Avl.h

--- header/Avl.h
+++ header/Avl.h
@@ -3,5 +3,11 @@
 #include "BST.h"
 
 class AVL : public BST {
-    
+    public:
+        int getHeight(Node*);
+        int getBalanceFactor(Node*);
+        bool isBalanced(Node*);
+        Node* rotateRight(Node*);
+        Node* rotateLeft(Node*);
+        Node* insert(Node*, std::string);
 };
