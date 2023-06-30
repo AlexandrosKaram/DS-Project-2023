492308b65a1c175a6fb81ede31f41563c1d8d6d6
iHaz32 <xaralamposdelig@gmail.com>
2023-07-01 01:25:31 +0300

    Modify AVL.h

--- header/AVL.h
+++ header/AVL.h
@@ -3,11 +3,22 @@
 #include "BST.h"
 
 class AVL : public BST {
-    protected:
+    public:
     int getHeight(Node*);
     int getBalanceFactor(Node*);
     bool isBalanced(Node*);
+
+    // rotations
     Node* rotateRight(Node*);
     Node* rotateLeft(Node*);
-    Node* insertAvlNode(Node*, Pair value);
+    Node* rightLeftRotate(Node*);
+    Node* leftRightRotate(Node*);
+
+    // inserts
+    void insertAvlNode(Node*&, Pair value);
+    void insert(Pair value);
+
+    void createPairs(std::string filename);
+    void searchPairs(Pair* Qset, int QsetSize);
+    void showResults(std::string filename, Pair* Qset, int QsetSize);
 };
