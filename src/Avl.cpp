e3c33c667f325dc6bb70324740eeb4ca5616bbd3
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-07-01 01:03:54 +0300

    Change avl to AVL.

--- src/Avl.cpp
+++ src/Avl.cpp
@@ -1,99 +1,99 @@
-// #include "../header/AVL.h"
+#include "../header/AVL.h"
 
-// int AVL::getHeight(Node* node) {
-//     if (node == nullptr) {
-//         return -1;
-//     }
-//     return node->height;
-// }
+int AVL::getHeight(Node* node) {
+    if (node == nullptr) {
+        return -1;
+    }
+    return node->height;
+}
 
-// int AVL::getBalanceFactor(Node* node) {
-//     if (node == nullptr) {
-//         return 0;
-//     }
-//     return getHeight(node->left) - getHeight(node->right);
-// }
+int AVL::getBalanceFactor(Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    return getHeight(node->left) - getHeight(node->right);
+}
 
-// bool AVL::isBalanced(Node* root) {
-//     if (root == nullptr) {
-//         return true;
-//     }
+bool AVL::isBalanced(Node* root) {
+    if (root == nullptr) {
+        return true;
+    }
 
-//     int balance = getBalanceFactor(root);
+    int balance = getBalanceFactor(root);
 
-//     if (std::abs(balance) > 1) {
-//         return false;
-//     }
+    if (std::abs(balance) > 1) {
+        return false;
+    }
 
-//     return isBalanced(root->left) && isBalanced(root->right);
-// }
+    return isBalanced(root->left) && isBalanced(root->right);
+}
 
-// Node* AVL::rotateRight(Node* node) {
-//     Node* newRoot = node->left;
-//     Node* temp = newRoot->right;
+Node* AVL::rotateRight(Node* node) {
+    Node* newRoot = node->left;
+    Node* temp = newRoot->right;
 
-//     newRoot->right = node;
-//     node->left = temp;
+    newRoot->right = node;
+    node->left = temp;
 
-//     node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
-//     newRoot->height = std::max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
+    node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+    newRoot->height = std::max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
 
-//     return newRoot;
-// }
+    return newRoot;
+}
 
-// Node* AVL::rotateLeft(Node* node) {
-//     Node* newRoot = node->right;
-//     Node* temp = newRoot->left;
+Node* AVL::rotateLeft(Node* node) {
+    Node* newRoot = node->right;
+    Node* temp = newRoot->left;
 
-//     newRoot->left = node;
-//     node->right = temp;
+    newRoot->left = node;
+    node->right = temp;
 
-//     node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
-//     newRoot->height = std::max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
+    node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+    newRoot->height = std::max(getHeight(newRoot->left), getHeight(newRoot->right)) + 1;
 
-//     return newRoot;
-// }
+    return newRoot;
+}
 
-// Node* AVL::insertAvlNode(Node* root, Pair value) {
-//     if (root == nullptr) {
-//         Node* newNode = new Node;
-//         newNode->value = value;
-//         newNode->left = nullptr;
-//         newNode->right = nullptr;
-//         newNode->height = 0;
-//         return newNode;
-//     }
+Node* AVL::insertAvlNode(Node* root, Pair value) {
+    if (root == nullptr) {
+        Node* newNode = new Node;
+        newNode->value = value;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        newNode->height = 0;
+        return newNode;
+    }
 
-//     if (value < root->value) {
-//         root->left = insert(root->left, value);
-//     } else if (value > root->value) {
-//         root->right = insert(root->right, value);
-//     } else {
-//         // Duplicate keys are not allowed in AVL tree
-//         return root;
-//     }
+    if (value < root->value) {
+        root->left = insert(root->left, value);
+    } else if (value > root->value) {
+        root->right = insert(root->right, value);
+    } else {
+        // Duplicate keys are not allowed in AVL tree
+        return root;
+    }
 
-//     root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
+    root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
 
-//     int balance = getBalanceFactor(root);
+    int balance = getBalanceFactor(root);
 
-//     if (balance > 1 && value < root->left->value) { // Right rotation
-//         return rotateRight(root);
-//     }
+    if (balance > 1 && value < root->left->value) { // Right rotation
+        return rotateRight(root);
+    }
 
-//     if (balance < -1 && value > root->right->value) { // Left rotation
-//         return rotateLeft(root);
-//     }
+    if (balance < -1 && value > root->right->value) { // Left rotation
+        return rotateLeft(root);
+    }
 
-//     if (balance > 1 && value > root->left->value) { // Left Right rotation
-//         root->left = rotateLeft(root->left);
-//         return rotateRight(root);
-//     }
+    if (balance > 1 && value > root->left->value) { // Left Right rotation
+        root->left = rotateLeft(root->left);
+        return rotateRight(root);
+    }
 
-//     if (balance < -1 && value < root->right->value) { // Right Left rotation
-//         root->right = rotateRight(root->right);
-//         return rotateLeft(root);
-//     }
+    if (balance < -1 && value < root->right->value) { // Right Left rotation
+        root->right = rotateRight(root->right);
+        return rotateLeft(root);
+    }
 
-//     return root;
-// }
+    return root;
+}
